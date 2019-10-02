1c771f6cdedce13fddf8265a569b7e2ca3582dff
Victor <ru.zhang1@ontariotechu.net>
2019-10-02 13:56:15 -0400

    Factory Pattern Template

--- Assets/Scripts/CommandPattern.cs
+++ Assets/Scripts/CommandPattern.cs
@@ -2,95 +2,98 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class CommandPattern : MonoBehaviour
+namespace CommandSpace
 {
-    public static List<Command> completedCommands = new List<Command>();
-    public static List<Command> undoneCommands = new List<Command>();
-    // Start is called before the first frame update
-    void Start()
+    public class CommandPattern : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-
-public abstract class Command
-{
-    public float newValue = 0;
-    public float oldValue = 0;
-
-    public abstract void Execute(Command command, float value);
-
-    public virtual void Undo() { }
-
-    public virtual void Redo() { }
-}
-
-public class ChangeValue : Command
-{
-    public override void Execute(Command command, float value)
-    {
-        // Do something
-
-        CommandPattern.completedCommands.Add(command);
-    }
-
-    public override void Undo()
-    {
-        // Reverse Command
-    }
-
-    public override void Redo()
-    {
-        // Redo Command
-    }
-}
-
-public class EmptyCommand : Command
-{
-    public override void Execute(Command command, float value)
-    {
-        // Do Nothing
-    }
-}
-
-public class UndoCommand : Command
-{
-    public override void Execute(Command command, float value)
-    {
-        int size = CommandPattern.completedCommands.Count;
-        if (size > 0)
+        public static List<Command> completedCommands = new List<Command>();
+        public static List<Command> undoneCommands = new List<Command>();
+        // Start is called before the first frame update
+        void Start()
         {
-            Command currentCommand = CommandPattern.completedCommands[size - 1];
 
-            currentCommand.Undo();
+        }
 
-            CommandPattern.completedCommands.RemoveAt(size - 1);
+        // Update is called once per frame
+        void Update()
+        {
 
-            CommandPattern.undoneCommands.Add(currentCommand);
         }
     }
-}
 
-public class RedoCommand : Command
-{
-    public override void Execute(Command command, float value)
+    public abstract class Command
     {
-        int size = CommandPattern.undoneCommands.Count;
-        if (size > 0)
+        public float newValue = 0;
+        public float oldValue = 0;
+
+        public abstract void Execute(Command command, float value);
+
+        public virtual void Undo() { }
+
+        public virtual void Redo() { }
+    }
+
+    public class ChangeValue : Command
+    {
+        public override void Execute(Command command, float value)
         {
-            Command currentCommand = CommandPattern.undoneCommands[size - 1];
+            // Do something
 
-            currentCommand.Redo();
+            CommandPattern.completedCommands.Add(command);
+        }
 
-            CommandPattern.undoneCommands.RemoveAt(size - 1);
+        public override void Undo()
+        {
+            // Reverse Command
+        }
 
-            CommandPattern.completedCommands.Add(currentCommand);
+        public override void Redo()
+        {
+            // Redo Command
+        }
+    }
+
+    public class EmptyCommand : Command
+    {
+        public override void Execute(Command command, float value)
+        {
+            // Do Nothing
+        }
+    }
+
+    public class UndoCommand : Command
+    {
+        public override void Execute(Command command, float value)
+        {
+            int size = CommandPattern.completedCommands.Count;
+            if (size > 0)
+            {
+                Command currentCommand = CommandPattern.completedCommands[size - 1];
+
+                currentCommand.Undo();
+
+                CommandPattern.completedCommands.RemoveAt(size - 1);
+
+                CommandPattern.undoneCommands.Add(currentCommand);
+            }
+        }
+    }
+
+    public class RedoCommand : Command
+    {
+        public override void Execute(Command command, float value)
+        {
+            int size = CommandPattern.undoneCommands.Count;
+            if (size > 0)
+            {
+                Command currentCommand = CommandPattern.undoneCommands[size - 1];
+
+                currentCommand.Redo();
+
+                CommandPattern.undoneCommands.RemoveAt(size - 1);
+
+                CommandPattern.completedCommands.Add(currentCommand);
+            }
         }
     }
 }
---
+++ Assets/Scripts/FactoryPattern.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FactorySpace
+{
+    public enum Types
+    {
+        Cube,
+        Sphere,
+        Cylinder
+    }
+
+    public class FactoryPattern : MonoBehaviour
+    {
+        public List<Spawn> objects = new List<Spawn>();
+        SpawnFactory Spawner = new SpawnFactory();
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                objects.Add(Spawner.GetSpawn(Types.Cube));
+
+                objects[0].Generate(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+            }
+        }
+    }
+
+    public abstract class Spawn
+    {
+        public abstract void Generate(float x, float y, float z);
+    }
+
+    public class SpawnCube : Spawn
+    {
+        public override void Generate(float x, float y, float z)
+        {
+            Debug.Log("Called");
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(x, y, z);
+        }
+    }
+
+    public class SpawnFactory
+    {
+        public Spawn GetSpawn(Types type)
+        {
+            switch(type)
+            {
+                case Types.Cube:
+                    return new SpawnCube();
+                default:
+                    return null;
+            }
+        }
+    }
+
+}
